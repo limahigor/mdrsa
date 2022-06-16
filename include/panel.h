bf6d38c81713f3d903152c29bab78a91d5ae5afc
limahigor <96789949+limahigor@users.noreply.github.com>
2022-06-16 11:52:41 -0300

    Update header panel

--- include/panel.h
+++ include/panel.h
@@ -1,17 +1,6 @@
 #ifndef PANEL_H
 #define PANEL_H
 
-#ifdef __unix__         
-    #include <stdlib.h>
-
-#elif defined(_WIN32) || defined(WIN32) 
-
-   #define OS_Windows
-
-   #include <windows.h>
-
-#endif
-
 #define LEN 1024
 
 #define AUTOFREE \
@@ -21,8 +10,8 @@ void cleanup_func(char **p);
 
 void main_panel();
 _Bool choose();
-void panel_chave();
-void panel_encriptar(list_t alfabeto);
-void panel_desencriptar();
+void panel_key();
+void panel_encrypt(list_t alfabeto);
+void panel_decrypt();
 
 #endif
